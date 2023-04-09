2334b5c1a6a334c8b8a7ba76ae7988e837783208
cameronLian <genxuan.lian@outlook.com>
2023-04-09 11:33:23 -0500

    Update hospital-occupancy.R

--- data-processing/hospital-occupancy.R
+++ data-processing/hospital-occupancy.R
@@ -1,6 +1,7 @@
 # Package prep
 require(tidyr)
 require(dplyr)
+require(tibble)
 require(readr)
 require(mosaic)
 require(ggplot2)
@@ -9,4 +10,12 @@ require(reshape2)
 # COVID-19 Reported Patient Impact and Hospital Capacity by Facility: https://healthdata.gov/Hospital/COVID-19-Reported-Patient-Impact-and-Hospital-Capa/anag-cw7u
 # COVID-19 Reported Patient Impact and Hospital Capacity by Facility (data source): https://healthdata.gov/stories/s/nhgk-5gpv
 patientImpact.hospitalCapacity <- read_csv("https://www.dropbox.com/s/fgb0ga3o84bfajp/covid-19_reported_patient_impact_and_hospital_capacity_by_facility.csv?dl=1")
+View(patientImpact.hospitalCapacity)
+
+patientImpact.hospitalCapacity$collection_week <- as.Date(patientImpact.hospitalCapacity$collection_week)
+class(patientImpact.hospitalCapacity$collection_week)
+
+week_number <- strftime(patientImpact.hospitalCapacity$collection_week, format = "%V")
+patientImpact.hospitalCapacity <- add_column(patientImpact.hospitalCapacity, 
+                                             week_number, .after = 1)
 View(patientImpact.hospitalCapacity)
