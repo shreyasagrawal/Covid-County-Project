64411654c4b0cdf0785579ebf17245dab07549ba
cameronLian <genxuan.lian@outlook.com>
2023-04-17 14:38:25 -0500

    data cleaning

--- data-processing/hospital-occupancy.R
+++ data-processing/hospital-occupancy.R
@@ -12,31 +12,24 @@ require(reshape2)
 patientImpact.hospitalCapacity <- read_csv("https://www.dropbox.com/s/fgb0ga3o84bfajp/covid-19_reported_patient_impact_and_hospital_capacity_by_facility.csv?dl=1")
 View(patientImpact.hospitalCapacity)
 
+# Convert collection_week to Date class
 patientImpact.hospitalCapacity$collection_week <- as.Date(patientImpact.hospitalCapacity$collection_week)
-class(patientImpact.hospitalCapacity$collection_week)
 
 # Create week_number column
 week_number <- strftime(patientImpact.hospitalCapacity$collection_week, format = "%V")
 patientImpact.hospitalCapacity <- add_column(patientImpact.hospitalCapacity, 
                                              week_number, .after = 1)
-View(patientImpact.hospitalCapacity)
 
 # Delete excessive variables
-# edit(patientImpact.hospitalCapacity) # XQuartz viewing table
-organized.capacity <- patientImpact.hospitalCapacity[, !grepl("influenza|pediatric|suspected|admission|vaccinated|ED", 
-                                          colnames(patientImpact.hospitalCapacity))] %>% 
-organized.capacity <- select(organized.capacity, -organized.capacity, -hospital_pk, -state, -collection_week, -ccn, 
-         -hospital_name, -address, -city, -zip, -hospital_subtype)
-all_adult_hospital_beds_7_day_avg, 
-total_adult_patients_hospitalized_confirmed_and_suspected_covid_7_day_avg, 
-total_pediatric_patients_hospitalized_confirmed_and_suspected_covid_7_day_avg, 
-staffed_icu_adult_patients_confirmed_and_suspected_covid_7_day_avg)
-View(organized.capacity)
-
-# patientImpact.hospitalCapacity <- as.data.frame(patientImpact.hospitalCapacity)
-# patientImpact.county <- aggregate(patientImpact.hospitalCapacity[, 4:ncol(patientImpact.hospitalCapacity)] ~ 
-#                                           fips_code + week_number, 
-#                                         na.omit(patientImpact.hospitalCapacity), mean)
-
-patientImpact.hospitalCapacity %>% 
-  group_by(fips_code, week_number) %>%
+strings.to.check <- c("organized.capacity", "hospital_pk", "state", 
+                      "ccn", "hospital_name", "address", "city", "zip", 
+                      "hospital_subtype", "pediatric", "influenza", "suspected", 
+                      "admission", "vaccinated", "ED", "is_metro_micro", 
+                      "hhs_ids", "is_corrected")
+for (string in strings.to.check) {
+  matching.vars <- grep(string, colnames(patientImpact.hospitalCapacity))
+  if (length(matching.vars) > 0) {
+    patientImpact.hospitalCapacity <-
+      patientImpact.hospitalCapacity[, -matching.vars]
+  }
+}
