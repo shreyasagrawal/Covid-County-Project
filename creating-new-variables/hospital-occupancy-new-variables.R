249b60ef19aba9a71a1fbb2ed254f46c5f71d876
cameronLian <genxuan.lian@outlook.com>
2023-04-18 13:56:06 -0500

    Update hospital-occupancy-new-variables.R

--- creating-new-variables/hospital-occupancy-new-variables.R
+++ creating-new-variables/hospital-occupancy-new-variables.R
@@ -7,4 +7,7 @@ require(mosaic)
 require(ggplot2)
 require(reshape2)
 
-dataset <- read_csv("https://www.dropbox.com/s/r3e5u7cz30ibe33/patient-impact-hospital-capacity-merged.csv?dl=0")
+# Dataset was processed in a separate document; new variables will be created
+# and calculated
+dataset <- read_csv("https://www.dropbox.com/s/r3e5u7cz30ibe33/patient-impact-hospital-capacity-merged.csv?dl=1")
+View(dataset)
