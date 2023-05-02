3e98d307dd2676b630fa1e3a90431b6e1feb0570
pomelohoho <52699926+pomelohoho@users.noreply.github.com>
2023-05-01 22:08:28 -0500

    Update places-project-chronic-diseases.R

--- data-processing/places-project-chronic-diseases.R
+++ data-processing/places-project-chronic-diseases.R
@@ -13,15 +13,15 @@ econ.2020 <- read.csv("https://www.dropbox.com/s/te81iz8nmg1q9ks/economic-charac
 econ.2021 <- read.csv("https://www.dropbox.com/s/q0iod9xnpjjdpcl/economic-characteristics-2021.csv?dl=1")
 
 ### Add week
-econ.2020 <-  econ.2020 %>% slice(rep(1:n(),52)) %>% mutate(Year = 2020, Week = rep(1:52,each = 3222))
-econ.2021 <-  econ.2021 %>% slice(rep(1:n(),52)) %>% mutate(Year = 2021, Week = rep(1:52,each = 3222))
+econ.2020 <-  econ.2020 %>% slice(rep(1:n(),52)) %>% mutate(`Year` = 2020, `Week` = rep(1:52,each = 3222))
+econ.2021 <-  econ.2021 %>% slice(rep(1:n(),52)) %>% mutate(`Year` = 2021, Week = rep(1:52,each = 3222))
 
 ### Combining all datasets above into a whole data set ----
 econ.all <- rbind(econ.2020, econ.2021)
 
 
 ### Choosing variables 
-econ.all <- econ.all %>% select('GEO_ID','NAME','DP03_0005E','DP03_0021E','DP03_0119PE','DP03_0095PE','DP03_0051PE','DP03_0042PE') 
+econ.all <- econ.all %>% select('GEO_ID','NAME','DP03_0001E','DP03_0005E','DP03_0021E','DP03_0119PE','DP03_0095PE','DP03_0051PE','DP03_0042PE','Year','Week') 
 
 ### Rename the columns, remove the first colummn
 colnames(econ.all) <- econ.all[1,]
@@ -32,11 +32,24 @@ names(econ.all)[names(econ.all) == 'Estimate!!COMMUTING TO WORK!!Workers 16 year
 names(econ.all)[names(econ.all) == 'Percent!!HEALTH INSURANCE COVERAGE!!Civilian noninstitutionalized population'] <- 'Health Insurance Coverage Percentage'
 names(econ.all)[names(econ.all) == 'Percent!!INCOME AND BENEFITS (IN 2020 INFLATION-ADJUSTED DOLLARS)!!Total households'] <- 'Household Income and Benefits'
 names(econ.all)[names(econ.all) == 'Percent!!INDUSTRY!!Civilian employed population 16 years and over!!Educational services, and health care and social assistance'] <- 'Edu Health Social Industry Percentage'
+names(econ.all)[names(econ.all) == 'Estimate!!EMPLOYMENT STATUS!!Population 16 years and over'] <- 'Population 16 and Over'
 
+econ.all$`Population 16 and Over` <- as.numeric(econ.all$`Population 16 and Over`)
+econ.all$`Estimate Unemployment` <- as.numeric(econ.all$`Estimate Unemployment`)
+econ.all$`Public Transport Commutation` <- as.numeric(econ.all$`Public Transport Commutation`)
+econ.all$`Household Income Below Poverty Level Percentage` <- as.numeric(econ.all$`Household Income Below Poverty Level Percentage`)
+econ.all$`Health Insurance Coverage Percentage` <- as.numeric(econ.all$`Health Insurance Coverage Percentage`)
+econ.all$`Edu Health Social Industry Percentage` <- as.numeric(econ.all$`Edu Health Social Industry Percentage`)
+econ.all <- econ.all %>% mutate(`Unemployment Rate` = `Estimate Unemployment`/`Population 16 and Over`,
+                                `Health Insurance Coverage Percentage` = `Health Insurance Coverage Percentage`/`Population 16 and Over`,
+                                `Public Transport Commutation` = `Public Transport Commutation`/`Population 16 and Over`)
 ### Check null
 #length(which(is.na(econ.all)))
 
 ### Add fips code 
 econ.all$Fips <- str_sub(econ.all$Geography,-5,-1)
 ###
-econ.all <- econ.all %>% separate(`Geographic Area Name`, sep=",", into = c("County", "State")) 
+econ.all$Year = econ.all$`2020`
+econ.all$Week = econ.all$`1`
+econ.all <- econ.all %>% select(!`2020`&!`1`)
+
