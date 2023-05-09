45aa1dd4912fdf53a3c039196b028ff58dbd79e6
cameronLian <genxuan.lian@outlook.com>
2023-05-08 22:55:10 -0500

    modeling

--- models/regression-model-background-script.R
+++ models/regression-model-background-script.R
@@ -14,88 +14,25 @@ library(corrplot)
 new_combined_2021 <- read_csv("https://www.dropbox.com/s/nolym5vik4je3yu/final_merged_data.csv?dl=1")
 
 new_combined_2021 <- new_combined_2021 %>% 
-  select(1:2, CFR, everything())
+  select(1:3, CFR, everything())
 
-# Large data frame into 4 smallers
-n <- ncol(new_combined_2021) - 4
-k <- 4
-cols_per_dataset <- ceiling(n / k)
-datasets <- list()
+new_combined_2021$cases <- NULL
+new_combined_2021$deaths <- NULL
+new_combined_2021 <- new_combined_2021[, -c(8:11)]
 
-for (i in seq_len(k)) {
-  start_col <- (i - 1) * cols_per_dataset + 5
-  end_col <- min(i * cols_per_dataset + 4, 
-                 ncol(new_combined_2021))
-  datasets[[i]] <- cbind(new_combined_2021[, 1:4], 
-                         new_combined_2021[, start_col:end_col])
-}
-quarter_1 <- datasets[[1]]
-quarter_2 <- datasets[[2]]
-quarter_3 <- datasets[[3]]
-quarter_4 <- datasets[[4]]
+df_nw <- new_combined_2021
+df_nw$white <- NULL # white variable is excluded in df_nw
+View(df_nw)
 
-# Stepwise regression models
+# Splitting into four smaller data frames
+# Determine the number of splits and the size of each split
+ncol(df_nw)
+constant <- df_nw[3:3]
+half_1 <- df_nw[4:17]
+half_2 <- df_nw[18:38]
+half_1 <- cbind(constant, half_1)
+half_2 <- cbind(constant, half_2)
 
-fit_1 <- lm(CFR ~ ., data = quarter_1)
-fit_2 <- lm(CFR ~ ., data = quarter_2)
-fit_3 <- lm(CFR ~ ., data = quarter_3)
-fit_4 <- lm(CFR ~ ., data = quarter_4)
-
-step_1 <- step(fit_1)
-step_2 <- step(fit_2)
-step_3 <- step(fit_3)
-step_4 <- step(fit_4)
-
-step_1
-step_2
-step_3
-step_4
-
-# Best subset regression models
-bs_1 <- regsubsets(data = quarter_1, 
-                   CFR ~ ., 
-                   nbest = 1, 
-                   nvmax = NULL, 
-                   force.in = NULL, force.out = NULL, 
-                   method = "exhaustive", 
-                   really.big = T)
-summary_bs_1 <- summary(bs_1)
-as.data.frame(summary_bs_1$outmat)
-numb_predictors_1 <- which.max(summary_bs_1$adjr2)
-summary_bs_1$which[numb_predictors_1,]
-
-bs_2 <- regsubsets(data = quarter_2, 
-                   CFR ~ ., 
-                   nbest = 1, 
-                   nvmax = NULL, 
-                   force.in = NULL, force.out = NULL, 
-                   method = "exhaustive", 
-                   really.big = T)
-summary_bs_2 <- summary(bs_2)
-as.data.frame(summary_bs_2$outmat)
-numb_predictors_2 <- which.max(summary_bs_2$adjr2)
-summary_bs_2$which[numb_predictors_2,]
-
-bs_3 <- regsubsets(data = quarter_3, 
-                   CFR ~ ., 
-                   nbest = 1, 
-                   nvmax = NULL, 
-                   force.in = NULL, force.out = NULL, 
-                   method = "exhaustive", 
-                   really.big = T)
-summary_bs_3 <- summary(bs_3)
-as.data.frame(summary_bs_3$outmat)
-numb_predictors_3 <- which.max(summary_bs_3$adjr2)
-summary_bs_3$which[numb_predictors_3,]
-
-bs_4 <- regsubsets(data = quarter_4, 
-                   CFR ~ ., 
-                   nbest = 1, 
-                   nvmax = NULL, 
-                   force.in = NULL, force.out = NULL, 
-                   method = "exhaustive", 
-                   really.big = T)
-summary_bs_4 <- summary(bs_4)
-as.data.frame(summary_bs_4$outmat)
-numb_predictors_4 <- which.max(summary_bs_4$adjr2)
-summary_bs_4$which[numb_predictors_4,]
+par(mar=c(1,1,1,1))
+pairs(half_1)
+pairs(half_2)
